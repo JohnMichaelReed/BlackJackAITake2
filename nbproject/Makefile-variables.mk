681520a1a6a259001bf4e85c8c8c3c3138a5c40a
JohnMichaelReed <johnmichaelreed@fas.harvard.edu>
2013-09-22 09:54:07 -0400

    Super_Simple_Version begin

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=blackjackai
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/blackjackai
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=blackjackai.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/blackjackai.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=blackjackai
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/blackjackai
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=blackjackai.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/blackjackai.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
